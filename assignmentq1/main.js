d184addaddd4c1d7cf5e36b23d08bc50c27f802f
unknown <pandeysn1997@gmail.com>
2022-05-04 07:52:55 +0530

    this is commit'

---
+++ assignmentq1/main.js
@@ -0,0 +1,9 @@
+alert("hello world");
+let a = prompt("your name !");
+let a1 = prompt("mention your staff id!");
+
+  
+document.getElementById("demo").innerHTML="okay";
+   document.getElementById("demo1").innerHTML="congralations you have passed the test";
+  
+
